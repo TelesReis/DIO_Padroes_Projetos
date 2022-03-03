08ae8eb19569be98e35953c95fc9bd9b139eb7d6
S3l3tC0rm <seletreis@gmail.com>
2022-03-03 16:25:31 -0300

    Initial

---
+++ src/main/java/com/padroesprojetos/gof/service/ClienteService.java
@@ -0,0 +1,17 @@
+package com.padroesprojetos.gof.service;
+
+import com.padroesprojetos.gof.model.Cliente;
+
+public interface ClienteService {
+
+	Iterable<Cliente> buscarTodos();
+
+	Cliente buscarPorId(Long id);
+
+	void inserir(Cliente cliente);
+
+	void atualizar(Long id, Cliente cliente);
+
+	void deletar(Long id);
+
+}
